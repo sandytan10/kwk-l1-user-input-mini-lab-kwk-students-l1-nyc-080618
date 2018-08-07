0fd5f73522616550d2b37b349940130a25d8b081
sandytan10 <stan10@stuy.edu>
2018-08-07 13:19:44 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,9 +1,10 @@
 # Code your prompts here!
 
 # First, puts out a string asking where the tourist would like to stay.
-stay = gets.chomp.capitalize
 
 puts "Let's plan your trip! Where would you like to #{stay}?"
+
+stay = gets.chomp.capitalize
 
 # Then, set a variable called stay to get the user's input. Make sure to capitilize that input.
 
@@ -11,12 +12,6 @@ puts "Let's plan your trip! Where would you like to #{stay}?"
 
 # Do the same thing with `eats` and one last time with `nights`, capturing their text input and storing them to the respective local variable
 
-puts "What would you like to #{eat}?"
-
-eat = gets.chomp.capitalize 
-
-puts "Where would you like to spend your #{nights}?"
-nights = gets.chomp.capitalize
 
 
 # Lastly, puts it all back by interpolating these values in a string.
